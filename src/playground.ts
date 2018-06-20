4b06fc36992e012cde298d734a8c12fa7aee47a0
Van Pho Nguyen <vanpho02@gmail.com>
2018-06-20 20:29:25 +0700

    init

---
+++ src/playground.ts
@@ -0,0 +1,37 @@
+console.log(1234);
+
+// number, string, boolean, null, undefined, void
+
+// function add(a: number, b: number): number {
+//     return a + b;
+// }
+// add(1, 4);
+// add(1, 2);
+
+// interface HasLength {
+//     length: number;
+// }
+// function getTotalLength(a: HasLength, b: HasLength): number {
+//     return a.length + b.length;
+// }
+// console.log(getTotalLength('abcd', [1, 2, 3]));
+
+interface Person {
+    name: string;
+    age: number;
+    height: number;
+}
+
+const teo: Person = { name: 'Teo Nguyen', age: 10, height: 100 };
+const ti: Person = { name: 'Ti Nguyen', age: 12, height: 90 };
+const tun = { name: 'Tun Nguyen', age: 15, height: 90 };
+
+const people: Person[] = [
+    { name: 'Teo Nguyen', age: 10, height: 100 },
+    { name: 'Ti Nguyen', age: 12, height: 90 },
+    { name: 'Tun Nguyen', age: 15, height: 90 }
+];
+
+const x: number[] = [1, 2, 3];
+
+let y: Person;
