8dac60038dd2c5f6d269ae6a2c0beaa4b42eef90
Van Pho Nguyen <vanpho02@gmail.com>
2018-06-20 20:47:02 +0700

    CanTalk

--- src/playground2.ts
+++ src/playground2.ts
@@ -1,1 +1,12 @@
-console.log('123');
+interface CanTalk {
+    talk: () => void;
+}
+
+class Person implements CanTalk {
+    constructor(private name: string, public age: number) {}
+
+    talk() { console.log('I am talking about Angular.'); }
+}
+
+const teo = new Person('Teo Nguyen', 10);
+console.log(teo.age);
